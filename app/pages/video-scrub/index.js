9f0a34dac37dbe526362fc27a65d27a40007b13b
Akbar Bakhshi Zanjani <akbzanj@gmail.com>
2022-03-20 13:21:38 -0400

    starter code commit

---
+++ app/pages/video-scrub/index.js
@@ -0,0 +1,24 @@
+import gsap from 'gsap'
+
+
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger)
+
+export default class VideoScrub{
+    constructor() {
+        this.setupScrollAnimation()
+    }
+    create() {
+    }
+    
+    animateIn() {
+    }
+
+    animateOut() {
+    }
+
+    setupScrollAnimation() {
+
+    }
+
+}
